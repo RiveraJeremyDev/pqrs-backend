466270171799428f0f38134ec9b5cd87c25ec768
Jeremy Rivera <jrc081128@gmail.com>
2026-02-26 20:14:53 -0500

    Conecto frontend con backend

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.config.database import engine, Base
 from app.routes import pqrs_routes
 from app.routes import conversacion_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -10,3 +11,15 @@ Base.metadata.create_all(bind=engine)
 app.include_router(pqrs_routes.router, prefix="/api")
 
 app.include_router(conversacion_routes.router, prefix="/api")
+
+origins = [
+    "https://pqrscul.netlify.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
